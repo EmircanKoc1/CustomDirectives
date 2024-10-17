6e1a0480fd64ceaea68a76b308b8563ab45483b5
Emircan <emircandv787@gmail.com>
2024-10-18 01:02:07 +0300

    The example1 has been refactored.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Example1Directive } from './directives/example1.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Example1Directive
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/directives/example1.directive.ts
+++ src/app/directives/example1.directive.ts
@@ -1,11 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appExample1]',
   standalone: true
 })
-export class Example1Directive {
+export class Example1Directive implements OnInit {
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+    element.nativeElement.style.backgroundColor = "red";
+  }
+  ngOnInit(): void {
+    this.element.nativeElement.style.backgroundColor = this.color;
+  }
 
+  @Input()
+  color!: string;
+
+  @HostListener("click")
+  onClick(): void {
+    alert("html nesnesi click edildi");
+  }
+
+  @HostBinding("style.color")
+  get textColor(): string {
+    return "white";
+  }
 }
